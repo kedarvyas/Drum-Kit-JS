ad1ddab38881562c3406b25566fa3b5b5a611fbf
kedarvyas <kedar.m.vyas@gmail.com>
2021-07-15 23:04:57 -0500

    moving js to main.js

---
+++ main.js
@@ -0,0 +1,21 @@
+function revertTransition(e) {
+    if (e.propertyName !== 'transform') {
+        return;
+    }
+    e.target.classList.remove('playing');
+}
+
+function startSound(e) {
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+    if (!audio) {
+        return;
+    }    
+    key.classList.add('playing');
+    audio.currentTime = 0;
+    audio.play();
+}
+
+const keys = Array.from(document.querySelectorAll('.key'));
+keys.forEach(key => key.addEventListener('transitionend', revertTransition));
+window.addEventListener('keydown', startSound);
